4c7ec47c4789280011a33735645dde55bded863f
Ahmed Nadir <28578413+TechnoVen@users.noreply.github.com>
2025-11-28 13:52:46 +0100

    Prepare rails app with external specs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Import Bootstrap JavaScript
+import { Popover } from "bootstrap"
+import * as bootstrap from "bootstrap"
+
+// Make Bootstrap available globally
+window.bootstrap = bootstrap
